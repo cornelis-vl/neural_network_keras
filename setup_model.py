4b28ee61b749b929ad792f6431e32d2342def641
cornelis-vl <cornelisvletter@gmail.com>
2017-03-17 15:54:55 +0100

    Changed setup script.

--- setup_model.py
+++ setup_model.py
@@ -8,34 +8,71 @@ from keras.optimizers import RMSprop
 from keras.utils.np_utils import to_categorical
 from get_kaggle_data import get_kaggle_mnist
 
-#Download data
-get_kaggle_mnist()
 
-#Read data
-train = pd.read_csv("train.csv")
-test_images = pd.read_csv("test.csv").values.astype('float32')
+# Read data
 
-#Pre processing
-train_images = (train.ix[:,1:].values).astype('float32')
-train_labels = train.ix[:,0].values.astype('int32')
+def vectorize_img_data(data):
+    dim1 = data.shape[0]
+    dim2 = data.shape[1] * data.shape[2]
 
-train_images = train_images.reshape(train_images.shape[0],  28, 28)
+    data_rsp = data.reshape((dim1, dim2))
 
-for i in range(6, 9):
-    plt.subplot(330 + (i+1))
-    plt.imshow(train_images[i], cmap=plt.get_cmap('gray'))
-    plt.title(train_labels[i]);
-
-train_images = train_images.reshape((42000, 28 * 28))
-
-train_images = train_images / 255
-test_images = test_images / 255
+    return data_rsp
 
 
-train_labels = to_categorical(train_labels)
-num_classes = train_labels.shape[1]
+def standardize_images(images, pixels=255):
+    return images / pixels
 
-#Design neural network
+
+def setup_data():
+    train = pd.read_csv("train.csv")
+    test_images = pd.read_csv("test.csv").values.astype('float32')
+
+    # Pre processing
+    train_images = train.ix[:, 1:].values.astype('float32')
+    train_labels = train.ix[:, 0].values.astype('int32')
+
+    train_images = train_images.reshape(train_images.shape[0], 28, 28)
+
+    train_features_raw = vectorize_img_data(train_images)
+    x_train = standardize_images(train_features_raw)
+    x_test = standardize_images(test_images)
+
+    y_train = to_categorical(train_labels)
+
+    return x_train, x_test, y_train
+
+
+def print_images(images, labels, numbers=range(6, 9)):
+    for i in numbers:
+        plt.subplot(330 + (i + 1))
+        plt.imshow(images[i], cmap=plt.get_cmap('gray'))
+        plt.title(labels[i])
+
+
+def run_neural_net(features, target):
+    num_features = features.shape[1]
+    mdl = Sequential()
+    mdl.add(Dense(32, activation='relu', input_dim=num_features))
+    mdl.add(Dense(16, activation='relu'))
+    mdl.add(Dense(10, activation='softmax'))
+
+    mdl.compile(optimizer=RMSprop(lr=0.001),
+                loss='categorical_crossentropy',
+                metrics=['accuracy'])
+
+    hist = mdl.fit(
+        features,
+        target,
+        validation_split=0.05,
+        nb_epoch=10,
+        batch_size=64
+    )
+
+    return mdl, hist
+
+
+# Design neural network
 
 # fix random seed for reproducibility
 seed = 43
@@ -49,7 +86,7 @@ model.add(Dense(32, activation='relu', input_dim=(28 * 28)))
 model.add(Dense(16, activation='relu'))
 model.add(Dense(10, activation='softmax'))
 
-#Compile network
+# Compile network
 
 model.compile(
     optimizer=RMSprop(lr=0.001),
@@ -65,9 +102,16 @@ history = model.fit(
     batch_size=64
 )
 
-#Predict
+# Predict
 predictions = model.predict_classes(test_images, verbose=0)
 
-submissions=pd.DataFrame({"ImageId": list(range(1,len(predictions)+1)),
-                         "Label": predictions})
+submissions = pd.DataFrame({"ImageId": list(range(1, len(predictions) + 1)),
+                            "Label": predictions})
 
+# Script
+
+get_kaggle_mnist()
+x_train, x_test, y_train = setup_data()
+
+nn, outpt = run_neural_net(features=x_train, target=y_train)
+predicts = nn.predict_classes(x_te, verbose=0)
